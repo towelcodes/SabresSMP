6d1148b74063f734b4ff6e07742fbd311f9bb443
towelsh <tea@towel.sh>
2025-08-11 11:40:24 +0100

    remove kotlin because its bad

--- src/main/kotlin/codes/towel/survivalSprint/effect/EffectType.kt
+++
@@ -1,7 +0,0 @@
-package codes.towel.survivalSprint.effect
-
-import kotlin.reflect.KClass
-
-enum class EffectType(val clazz: KClass<out Effect>) {
-    TotemCooldown(TotemCooldownEffect::class)
-}
--- src/main/kotlin/codes/towel/survivalSprint/util/Ref.kt
+++
@@ -1,3 +0,0 @@
-package codes.towel.survivalSprint.util
-
-class Ref<T>(var value: T)
